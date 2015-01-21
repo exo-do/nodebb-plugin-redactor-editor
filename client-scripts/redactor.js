a64c3d80d396bbe39b15de6f94913e39a6f3299e
xusoo <xusoo@users.noreply.github.com>
2015-01-21 21:27:13 +0100

    Fixes para NodeBB 0.6.x
    Y des-minificado

--- client-scripts/redactor.js
+++ client-scripts/redactor.js
@@ -1,1 +1,15 @@
-$(document).ready(function(){return $(window).on("action:composer.loaded",function(){return $(".composer textarea").attr("name","composer").redactor({tabindex:3,allowedTags:["h3","h4","h5","h6","blockquote","p","a","ul","ol","nl","li","b","img","i","strong","em","strike","code","hr","br","div","table","thead","caption","tbody","tr","th","td","pre","iframe","video","source"],buttonSource:!1,convertVideoLinks:!0,buttons:["formatting","bold","italic","deleted","unorderedlist","orderedlist","outdent","indent","image","video","link","horizontalrule"],initCallback:function(){return $(".redactor_box").addClass("topic"),$(".redactor_editor").addClass("topic-text")}})})});
+$(document).ready(function () {
+	return $(window).on("action:composer.loaded", function () {
+		return $(".composer textarea").attr("name", "composer").redactor({
+			tabindex: 3,
+			allowedTags: ["h1", "h2", "h3", "h4", "h5", "h6", "blockquote", "p", "a", "ul", "ol", "nl", "li", "b", "img", "i", "strong", "em", 
+						  "strike", "code", "hr", "br", "div", "table", "thead", "caption", "tbody", "tr", "th", "td", "pre", "iframe", "video", "source"],
+			buttonSource: true,
+			convertVideoLinks: !0,
+			buttons: ["html", "formatting", "bold", "italic", "deleted", "unorderedlist", "orderedlist", "outdent", "indent", "image", "video", "link", "horizontalrule"],
+			initCallback: function () {
+				return $(".redactor_box").addClass("topic"), $(".redactor_editor").addClass("topic-text"), $(".composer").addClass("redactor")
+			}
+		})
+	})
+});
